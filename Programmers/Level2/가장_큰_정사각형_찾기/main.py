c3e421432dcacac8d90634ffadbee0b4b4576204
parkyolo <20181621@kookmin.ac.kr>
2021-08-17 16:39:33 +0900

    Update code

--- Programmers/Level2/가장_큰_정사각형_찾기/main.py
+++ Programmers/Level2/가장_큰_정사각형_찾기/main.py
@@ -4,7 +4,7 @@ def solution(board):
     for i in range(1, len(board)):
         for j in range(1, len(board[0])):
             if board[i][j] == 1:
-                board[i][j] = min(board[i-1][j-1], min(board[i-1][j], board[i][j-1])) + 1
+                board[i][j] = min(board[i-1][j-1], board[i-1][j], board[i][j-1]) + 1
         
     for row in board:
         if max(row) > answer:
