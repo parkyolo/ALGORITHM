23667b7ef6d30322f83d36d0043aa191812cb71f
parkyolo <20181621@kookmin.ac.kr>
2021-08-18 15:04:38 +0900

    Add annotation

--- Programmers/Level2/[3차]방금_그곡/main.py
+++ Programmers/Level2/[3차]방금_그곡/main.py
@@ -11,17 +11,19 @@ def solution(m, musicinfos):
     answer = ('(None)',0)
 
     for info in musicinfos:
-        start_time, end_time, title, tune = info.split(',')
+        start_time, end_time, title, music = info.split(',')
         s_f, s_b = map(int,start_time.split(":"))
         e_f, e_b = map(int,end_time.split(":"))
 
-        time = 0
+        time = 0 # 재생 시간
         if e_b > s_b: time = e_b-s_b + 60*(e_f-s_f)
         else: time = 60*(e_f-s_f)-s_b+e_b
 
-        tune = change(tune)
-        music_played = (tune*(time//len(tune)+1))[:time]
+        music = change(music)
+        # 재싱된 곡
+        music_played = (music*(time//len(music)+1))[:time]
 
+        # 네오가 기억한 멜로디와 일치하는 곡일 경우
         if m in music_played:
             if answer[1] == '(None)' or time > answer[1]:
                 answer = (title, time)
