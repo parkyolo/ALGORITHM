c461c0ea7d4c3e1e8656450aea571fe2a8fbdea9
parkyolo <20181621@kookmin.ac.kr>
2022-02-26 20:19:40 +0900

    Baekjoon 12865 평범한 배낭

---
+++ Baekjoon/GOLD5/12865_평범한배낭_dp_kanpsack/main.py
@@ -0,0 +1,18 @@
+n, k = map(int, input().split())
+things = [list(map(int, input().split())) for _ in range(n)]
+things = [[0,0]]+things
+things.sort() # 무게 순으로 정렬
+
+knapsack = [[0 for _ in range(k+1)] for _ in range(n+1)]
+
+for i in range(1, n+1): # i : 배낭에 넣을 물건의 index (1~n)
+    for j in range(k+1): # j : 배낭에 넣을 수 있는 물건의 무게
+        if j == 0: continue
+        elif j >= things[i][0]: # i번째 물건이 배낭에 들어갈 수 있을 경우
+            # i번째 물건인 things[i]를 넣는 경우 --> things[i]의 가치 + 이전 배낭에서 j-things[i]의 무게만큼 덜 넣었을 때의 가치
+            # 안넣는 경우 --> 이전 배낭인 knapsack[i-1]에 j만큼의 무게를 넣었을 때의 가치인 knapsack[i-1][j]
+            # 둘 중 큰 값으로 갱신
+            knapsack[i][j] = max(things[i][1]+knapsack[i-1][j-things[i][0]], knapsack[i-1][j])
+        else:
+            knapsack[i][j] = knapsack[i-1][j]
+print(knapsack[-1][-1])
