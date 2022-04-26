9d4b7ce459b38ab5b82d9e5fe822e03cdbb93f66
parkyolo <20181621@kookmin.ac.kr>
2022-04-26 14:18:30 +0900

    조합 요리사

---
+++ SWExpertAcademy/4012_cook/main.py
@@ -0,0 +1,41 @@
+T = int(input())
+
+for test_case in range(1, T + 1):
+    N = int(input())
+    synergy = list(list(map(int, input().strip().split())) for _ in range(N))
+
+    def cal(arr): # 시너지 계산
+        global synergy
+        res = 0
+        for i in range(len(arr)-1):
+            for j in range(i+1, len(arr)):
+                res += synergy[arr[i]][arr[j]]
+                res += synergy[arr[j]][arr[i]]
+        return res
+
+    def comb(idx, n): # a의 조합 구하기
+        global arr, a_arr
+
+        if n == N//2:
+            a = [i for i in range(N) if arr[i]]
+            return a_arr.append(a)
+
+        for i in range(idx, N):
+            if arr[i]: continue
+            arr[i] = 1
+            comb(i, n+1)
+            arr[i] = 0
+
+    arr = [0 for _ in range(N)]
+    a_arr = []
+    comb(0, 0)
+
+    min_diff = 2147483648
+    for a in a_arr: # 시너지 차이의 최솟값 구하기
+        b = [i for i in range(N) if i not in a]
+        a_synergy = cal(a)
+        b_synergy = cal(b)
+        diff = max(a_synergy, b_synergy) - min(a_synergy, b_synergy)
+        min_diff = min(min_diff, diff)
+
+    print('#'+str(test_case)+' '+str(min_diff))
