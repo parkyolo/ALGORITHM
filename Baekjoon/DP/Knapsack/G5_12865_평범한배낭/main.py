06738547712fac4ffd6437d365005f2e56886e1e
parkyolo <20181621@kookmin.ac.kr>
2022-04-29 20:58:54 +0900

    배낭 문제 1차원 배열로 풀이

--- Baekjoon/DP/Knapsack/G5_12865_평범한배낭/main.py
+++ Baekjoon/DP/Knapsack/G5_12865_평범한배낭/main.py
@@ -6,9 +6,8 @@ things.sort() # 무게 순으로 정렬
 knapsack = [[0 for _ in range(k+1)] for _ in range(n+1)]
 
 for i in range(1, n+1): # i : 배낭에 넣을 물건의 index (1~n)
-    for j in range(k+1): # j : 배낭에 넣을 수 있는 물건의 무게
-        if j == 0: continue
-        elif j >= things[i][0]: # i번째 물건이 배낭에 들어갈 수 있을 경우
+    for j in range(1, k+1): # j : 배낭에 넣을 수 있는 물건의 무게
+        if j >= things[i][0]: # i번째 물건이 배낭에 들어갈 수 있을 경우
             # i번째 물건인 things[i]를 넣는 경우 --> things[i]의 가치 + 이전 배낭에서 j-things[i]의 무게만큼 덜 넣었을 때의 가치
             # 안넣는 경우 --> 이전 배낭인 knapsack[i-1]에 j만큼의 무게를 넣었을 때의 가치인 knapsack[i-1][j]
             # 둘 중 큰 값으로 갱신
@@ -16,3 +15,16 @@ for i in range(1, n+1): # i : 배낭에 넣을 물건의 index (1~n)
         else:
             knapsack[i][j] = knapsack[i-1][j]
 print(knapsack[-1][-1])
+
+
+"""
+1차원 배열을 이용한 풀이
+물건이 하나씩만 있을 때에는 큰 배낭부터(무게가 K일 때부터) 넣어준다.
+
+knapsack = [0 for _ in range(K+1)]
+for w, v in things:
+    for j in range(K, w-1, -1):
+        knapsack[j] = max(knapsack[j-w]+v, knapsack[j])
+print(knapsack[K])
+
+"""
--- Baekjoon/DP/Knapsack/S2_1535_안녕/main.py
+++ Baekjoon/DP/Knapsack/S2_1535_안녕/main.py
@@ -14,3 +14,23 @@ for i in range(n):
             dp[i][j] = dp[i - 1][j]
 
 print(dp[-1][-1])
+
+"""
+1차원 배열로 풀이
+체력이 큰 순서대로 기쁨을 sp 배열에 넣어준다.
+
+N = int(input())
+strength = list(map(int, input().split()))
+pleasure = list(map(int, input().split()))
+
+sp = [[s, p] for s, p in zip(strength, pleasure)]
+sp.sort()
+
+hp = [0 for _ in range(100)]
+for s, p in sp:
+    for j in range(99, s-1, -1):
+        hp[j] = max(hp[j-s]+p, hp[j])
+
+
+print(hp[99])
+"""
