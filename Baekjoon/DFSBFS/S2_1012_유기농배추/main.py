149714d9efe03735cf88e2b1f4a3bbd79cc3213b
parkyolo <39394642+parkyolo@users.noreply.github.com>
2023-06-17 14:10:12 +0900

    방문체크 방식 수정

--- Baekjoon/DFSBFS/S2_1012_유기농배추/main.py
+++ Baekjoon/DFSBFS/S2_1012_유기농배추/main.py
@@ -1,30 +1,31 @@
 from collections import deque
 
 t = int(input())
+dxy = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+
 for _ in range(t):
-    m, n , k = map(int, input().split())
-    land = [[0 for _ in range(m)] for _ in range(n)]
-    v = [[0 for _ in range(m)] for _ in range(n)] # 방문 확인
+    m, n, k = map(int, input().split())
+    board = [[0 for _ in range(m)] for _ in range(n)]
 
     for _ in range(k):
-        i, j = map(int, input().split())
-        land[j][i] = 1
-    
-    dx = [-1, 1, 0, 0]
-    dy = [0, 0, -1, 1]
+        x, y = map(int, input().split())
+        board[y][x] = 1
 
     answer = 0
-    for i in range(n):
-        for j in range(m):
-            if land[i][j] == 1 and v[i][j] == 0: # 아직 방문하지 않은 위치에 배추가 있을 때
-                queue = deque()
-                queue.append([i,j])
-                while queue:
-                    x, y = queue.popleft()
-                    for d in range(4): # 상하좌우에 인접한 배추가 있는지 탐색
-                        if 0 <= x + dx[d] < n and 0 <= y + dy[d] < m:
-                            if land[x + dx[d]][y + dy[d]] == 1 and v[x + dx[d]][y + dy[d]] == 0:
-                                queue.append([x + dx[d], y + dy[d]])
-                                v[x + dx[d]][y + dy[d]] = 1 # 방문 체크
+    for y in range(n):
+        for x in range(m):
+            if board[y][x]: # 아직 방문하지 않은 위치에 배추가 있을 때
                 answer += 1
+                queue = deque([(y, x)])
+                board[y][x] = 0
+                
+                while queue:
+                    cy, cx = queue.popleft()
+                    for d in range(4): # 상하좌우에 인접한 배추가 있는지 탐색
+                        ny, nx = cy + dxy[d][0], cx + dxy[d][1]
+                        if ny < 0 or ny >= n or nx < 0 or nx >= m: continue
+                        if not board[ny][nx]: continue
+                        queue.append((ny, nx))
+                        board[ny][nx] = 0
+                
     print(answer)
