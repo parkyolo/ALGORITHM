e05d5cfc08fcc038dc83b605bf4dbeb45f3f2c8b
parkyolo <39394642+parkyolo@users.noreply.github.com>
2023-06-17 14:36:11 +0900

    변수명 수정

--- Baekjoon/DFSBFS/S2_2644_촌수계산/main.py
+++ Baekjoon/DFSBFS/S2_2644_촌수계산/main.py
@@ -4,24 +4,27 @@ n = int(input())
 s, e = map(int, input().strip().split())
 m = int(input())
 
-rel = [[] for _ in range(n)]
+rel = [[] for _ in range(n+1)]
 for _ in range(m):
     p, c = map(int, input().strip().split())
-    rel[p-1].append(c-1)
-    rel[c-1].append(p-1)
+    rel[p].append(c)
+    rel[c].append(p)
 
-v = set()
-queue = deque()
-queue.append([s-1, 0])
-v.add(s-1)
+queue = deque([(s, 0)]) # 시작 노드, 거리
+visited = set()         # 방문 체크 셋
+visited.add(s)
 
+answer = -1
 while queue:
-    cur, cnt = queue.popleft()
-    if cur+1 == e:
-        print(cnt)
-        exit()
-    for f in rel[cur]:
-        if f not in v:
-            v.add(f)
-            queue.append([f, cnt + 1])
-print(-1)
+    cur_node, cnt = queue.popleft()
+    
+    if cur_node == e:
+        answer = cnt
+        break
+    
+    for next_node in rel[cur_node]:
+        if next_node not in visited:
+            visited.add(next_node)
+            queue.append((next_node, cnt + 1))
+
+print(answer)
