0a392891961e3a562c84cb597280fe23995e1151
parkyolo <39394642+parkyolo@users.noreply.github.com>
2023-06-17 13:41:31 +0900

    2차원 배열 -> linked list로 풀이 수정

--- Baekjoon/DFSBFS/S3_2606_바이러스/main.py
+++ Baekjoon/DFSBFS/S3_2606_바이러스/main.py
@@ -1,26 +1,26 @@
 from collections import deque
 
-n = int(input())
-connect = int(input())
-v = [0 for _ in range(n)]
-v[0] = 1
-network = [[0 for _ in range(n)] for _ in range(n)]
-for _ in range(connect):
-    i,j = map(int, input().split())
-    network[i-1][j-1] = 1
-    network[j-1][i-1] = 1
-queue = deque()
-answer = 0
-for i in range(n):
-    if network[0][i] == 1:
-        queue.append(i)
+n = int(input())  # 컴퓨터의 수
+e = int(input())  # 연결된 컴퓨터 쌍의 수
+
+visited = [0 for _ in range(n+1)]   # 방문 체크 배열
+visited[1] = 1                      # 1번 컴퓨터부터 탐색 시작
+
+network = [[] for _ in range(n+1)]  # 연결 리스트
+
+# 연결된 컴퓨터의 번호를 연결 리스트에 추가
+for _ in range(e):
+    a, b = map(int, input().split())
+    network[a].append(b)
+    network[b].append(a)
+
+# 1번 컴퓨터와 연결된 컴퓨터 탐색
+queue = deque([1])
 while queue:
-    i = queue.popleft()
-    if v[i] == 1: continue
-    v[i] = 1
-    answer += 1
-    for j in range(n):
-        if v[j] != 1:
-            if network[i][j] ==1:
-                queue.append(j)
-print(answer)
+    cur_com = queue.popleft()
+    for next_com in network[cur_com]:
+        if not visited[next_com]:
+            visited[next_com] = 1
+            queue.append(next_com)
+            
+print(sum(visited)-1) # 1번 컴퓨터를 제외하고 1번 컴퓨터와 연결된 컴퓨터의 수 출력
